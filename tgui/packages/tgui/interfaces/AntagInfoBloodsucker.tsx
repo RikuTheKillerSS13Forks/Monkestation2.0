931a00a637f28ee1ee1fd7ea83dd55ea3b5bde51
RikuTheKiller <88713943+RikuTheKiller@users.noreply.github.com>
2024-08-23 23:19:31 +0300

    decomment

--- tgui/packages/tgui/interfaces/AntagInfoBloodsucker.tsx
+++ tgui/packages/tgui/interfaces/AntagInfoBloodsucker.tsx
@@ -1,5 +1,3 @@
-/* ONGOING VAMPIRE REWORK, FIX THIS ONCE YOU KNOW WTF TO DO WITH IT
-
 import { resolveAsset } from '../assets';
 import { BooleanLike } from 'common/react';
 import { useBackend, useLocalState } from '../backend';
@@ -274,4 +272,3 @@ const PowerSection = (props: any) => {
     </Section>
   );
 };
-*/
